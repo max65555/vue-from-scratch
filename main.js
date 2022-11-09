c5976912e86a8fe36215aea019549b1dd26666d5
max6555 <khanhtoan.le.6555@gmail.com>
2022-11-09 11:59:49 +0700

    update components & components props

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// let app = Vue.createApp({
+function log(message) {
+	console.log(message);
+} // let app = Vue.createApp({
 // 	data: function () {
 // 		return {
 // 			message: 'Hello World',
@@ -9,26 +11,56 @@
 // 		};
 // 	},
 // });
-function log(message) {
-	console.log(message);
-}
-let app = Vue.createApp({
+
+// let app = Vue.createApp({
+// 	data: function () {
+// 		return {
+// 			evMeBox: true,
+// 			greeting: '',
+// 			visibleInput: true,
+
+// 		};
+// 	},
+// 	methods: {
+// 		OnClickEvent: function () {
+// 			this.evMeBox = !this.evMeBox;
+// 		},
+// 		keyUpEvent(object) {
+// 			log(object.target);
+// 			this.visibleInput = false;
+// 		},
+// 	},
+// });
+let app = Vue.createApp();
+app.component('login-form', {
+	template: `
+	<h1>{{greeting}}</h1>
+	<form v-on:submit.prevent="HandleSubmit" class="center-box">
+	<input type="text" placeholder="username" v-model=userName>		
+	<input type="text" placeholder="password" v-model=password>		
+	<button >Login</button>		
+	</form>
+	`,
 	data: function () {
 		return {
-			evMeBox: true,
-			greeting: '',
-			visibleInput: true,
+			userName: '',
+			password: '',
 		};
 	},
 	methods: {
-		OnClickEvent: function () {
-			this.evMeBox = !this.evMeBox;
+		CheckLogin: function () {
+			log(this.userName + ' __ ' + this.password);
 		},
-		keyUpEvent(object) {
-			log(object.target);
-			this.visibleInput = false;
+		HandleSubmit: function () {
+			console.log('Submit Form');
 		},
-		// app.component();
 	},
+});
+app.component('custom-input', {
+	template: `
+		<div class="component__custom-input--container">
+			<input type="text" v-model="input_data" placeholder="placeholder" class="component__custom-input--input">
+		</div>
+	`,
 });
 app.mount('#app');
